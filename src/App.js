05b59b0716e725e811ff373e32b21c3768f18e62
ImJaePang <narushel@naver.com>
2024-06-30 17:41:15 +0900

    coinTracker complete

--- src/App.js
+++ src/App.js
@@ -4,6 +4,23 @@ import {useState, useEffect} from "react";
 function App() {
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
+  const [usd, setUsd] = useState(0);
+  const [exchange, setExchange] = useState(0);
+
+  function onchangeUsd(usd) {
+    setUsd(usd.target.value);
+    const conis = document.querySelector("#conis");
+    const targetCoin = conis.options[conis.selectedIndex];
+    const targetPrice = targetCoin.getAttribute("price");
+    setExchange(usd.target.value /targetPrice);
+  }
+
+  function onChangeCoins(){
+    const conis = document.querySelector("#conis");
+    const targetCoin = conis.options[conis.selectedIndex];
+    const targetPrice = targetCoin.getAttribute("price");
+    setExchange(usd/targetPrice);
+  }
 
   useEffect(()=>{
     fetch("https://api.coinpaprika.com/v1/tickers")
@@ -17,16 +34,23 @@ function App() {
   return (
     <div>
       <h1>The coins! ({loading ? null : coins.length}) </h1>
+      <div>
+        $ <input onChange={onchangeUsd} type="text" value={usd}/> usd
+        <div>
+          <h3> exchange :  {exchange} 개 구입 가능</h3>
+        </div>
+      </div>
+
       {
       loading ?
         <h3> loading... </h3>
-        : <ul>
+        : <select id="conis" onChange={onChangeCoins}>
             {coins.map((coin) => 
-              <li key={coin.id}>
+              <option key={coin.id} name={coin.name} price={coin.quotes.USD.price}>
                 {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price} USD
-              </li>)
+              </option>)
             }
-          </ul>
+          </select>
       }
     </div>
   );

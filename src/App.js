34e256ad13b651bc448d703462c6c155016155c2
ImJaePang <narushel@naver.com>
2024-06-30 17:02:55 +0900

    lecture practice

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from "react";
+
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+
+  useEffect(()=>{
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>The coins! ({loading ? null : coins.length}) </h1>
+      {
+      loading ?
+        <h3> loading... </h3>
+        : <ul>
+            {coins.map((coin) => 
+              <li key={coin.id}>
+                {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price} USD
+              </li>)
+            }
+          </ul>
+      }
     </div>
   );
 }
